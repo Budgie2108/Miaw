5136ef8396bae2a0082d83b38a67a50109a1177c
RobDaMob <70642816+RobDaMob@users.noreply.github.com>
2020-09-02 14:00:21 +0100

    Setup
    My Setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='Kitty',
+      version='0.1',
+      description='A module full of functions',
+      url='https://github.com/RobDaMob/Kitten/',
+      author='Rob The Mob',
+      author_email='RobWork@gmail.com',
+      license='MIT',
+      packages=['Kitty'],
+      zip_safe=False)
